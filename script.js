cadacd8dc2edc9e9ce2ccdeb643e0be2c0f0ad73
WagnerDev9 <99822078+WagnerDev9@users.noreply.github.com>
2022-03-11 01:59:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,279 @@
+/*------------------------------------------*/
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    iframe.classList.add('.iframe');
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (event) => {
+        if (event.target.id == modalID || event.target.className == 'fechar') {
+            modal.classList.remove('mostrar');
+        }
+
+    })
+}
+
+const a1 = document.querySelector('.a1');
+a1.addEventListener('click', () => iniciaModal('modal1'));
+
+$('#modal1.modal-container').on('click', function () {
+
+    var video = $(".iframe1").attr("src");
+    $(".iframe1").attr("src", "");
+    $(".iframe1").attr("src", video);
+
+});
+
+
+/*----------------------------------------------*/
+
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == modalID || e.target.className == 'fechar') {
+            modal.classList.remove('mostrar');
+        }
+    })
+}
+const a2 = document.querySelector('.a2');
+a2.addEventListener('click', () => iniciaModal('modal2'));
+
+$('#modal2.modal-container').on('click', function () {
+
+    var video = $(".iframe2").attr("src");
+    $(".iframe2").attr("src", "");
+    $(".iframe2").attr("src", video);
+
+});
+
+
+
+
+/*----------------------------------------------*/
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == modalID || e.target.className == 'fechar') {
+            modal.classList.remove('mostrar');
+        }
+    })
+}
+const a3 = document.querySelector('.a3');
+a3.addEventListener('click', () => iniciaModal('modal3'))
+
+$('#modal3.modal-container').on('click', function () {
+
+    var video = $(".iframe3").attr("src");
+    $(".iframe3").attr("src", "");
+    $(".iframe3").attr("src", video);
+
+});
+
+
+
+/*----------------------------------------------*/
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == modalID || e.target.className == 'fechar') {
+            modal.classList.remove('mostrar');
+        }
+    })
+}
+const a4 = document.querySelector('.a4');
+a4.addEventListener('click', () => iniciaModal('modal4'))
+
+$('#modal4.modal-container').on('click', function () {
+
+    var video = $(".iframe4").attr("src");
+    $(".iframe4").attr("src", "");
+    $(".iframe4").attr("src", video);
+
+});
+
+/*----------------------------------------------*/
+/*CHILL*/
+/*----------------------------------------------*/
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == modalID || e.target.className == 'fechar') {
+            modal.classList.remove('mostrar');
+        }
+    })
+}
+const b1 = document.querySelector('.b1');
+b1.addEventListener('click', () => iniciaModal('modalb1'))
+
+$('#modalb1.modal-container').on('click', function () {
+
+    var video = $(".iframeb1").attr("src");
+    $(".iframeb1").attr("src", "");
+    $(".iframeb1").attr("src", video);
+
+});
+
+/*----------------------------------------------*/
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == modalID || e.target.className == 'fechar') {
+            modal.classList.remove('mostrar');
+        }
+    })
+}
+const b2 = document.querySelector('.b2');
+b2.addEventListener('click', () => iniciaModal('modalb2'))
+
+$('#modalb2.modal-container').on('click', function () {
+
+    var video = $(".iframeb2").attr("src");
+    $(".iframeb2").attr("src", "");
+    $(".iframeb2").attr("src", video);
+
+});
+
+
+/*----------------------------------------------*/
+
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == modalID || e.target.className == 'fechar') {
+            modal.classList.remove('mostrar');
+        }
+    })
+}
+const b3 = document.querySelector('.b3');
+b3.addEventListener('click', () => iniciaModal('modalb3'))
+
+$('#modalb3.modal-container').on('click', function () {
+
+    var video = $(".iframeb3").attr("src");
+    $(".iframeb3").attr("src", "");
+    $(".iframeb3").attr("src", video);
+
+});
+
+/*----------------------------------------------*/
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == modalID || e.target.className == 'fechar') {
+            modal.classList.remove('mostrar');
+        }
+    })
+}
+const b4 = document.querySelector('.b4');
+b4.addEventListener('click', () => iniciaModal('modalb4'))
+
+$('#modalb4.modal-container').on('click', function () {
+
+    var video = $(".iframeb4").attr("src");
+    $(".iframeb4").attr("src", "");
+    $(".iframeb4").attr("src", video);
+
+});
+
+/*----------------------------------------------*/
+/*LOVE*/
+/*----------------------------------------------*/
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == modalID || e.target.className == 'fechar') {
+            modal.classList.remove('mostrar');
+        }
+    })
+}
+const c1 = document.querySelector('.c1');
+c1.addEventListener('click', () => iniciaModal('modalc1'))
+
+$('#modalc1.modal-container').on('click', function () {
+
+    var video = $(".iframec1").attr("src");
+    $(".iframec1").attr("src", "");
+    $(".iframec1").attr("src", video);
+
+});
+
+/*----------------------------------------------*/
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == modalID || e.target.className == 'fechar') {
+            modal.classList.remove('mostrar');
+        }
+    })
+}
+const c2 = document.querySelector('.c2');
+c2.addEventListener('click', () => iniciaModal('modalc2'))
+
+$('#modalc2.modal-container').on('click', function () {
+
+    var video = $(".iframec2").attr("src");
+    $(".iframec2").attr("src", "");
+    $(".iframec2").attr("src", video);
+
+});
+
+/*----------------------------------------------*/
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == modalID || e.target.className == 'fechar') {
+            modal.classList.remove('mostrar');
+        }
+    })
+}
+const c3 = document.querySelector('.c3');
+c3.addEventListener('click', () => iniciaModal('modalc3'))
+
+$('#modalc3.modal-container').on('click', function () {
+
+    var video = $(".iframec3").attr("src");
+    $(".iframec3").attr("src", "");
+    $(".iframec3").attr("src", video);
+
+});
+
+/*----------------------------------------------*/
+
+function iniciaModal(modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == modalID || e.target.className == 'fechar') {
+            modal.classList.remove('mostrar');
+        }
+    })
+}
+const c4 = document.querySelector('.c4');
+c4.addEventListener('click', () => iniciaModal('modalc4'))
+
+$('#modalc4.modal-container').on('click', function () {
+
+    var video = $(".iframec4").attr("src");
+    $(".iframec4").attr("src", "");
+    $(".iframec4").attr("src", video);
+
+});
